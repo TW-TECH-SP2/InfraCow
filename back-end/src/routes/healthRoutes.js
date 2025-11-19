57c38f43340acff8245375a78a25a2a2af24e262
rafdavis <rd506269@gmail.com>
2025-11-18 21:50:25 -0300

    fix: Correção da rota health

--- back-end/src/routes/healthRoutes.js
+++ back-end/src/routes/healthRoutes.js
@@ -3,7 +3,7 @@ import express from 'express'
 const healthRoutes = express.Router();
 
 healthRoutes.get('/health', (req, res) => {
-  res.status(200).send('OK');
+  res.status(200).json({ status: 'OK' });
 });
 
 export default healthRoutes;
